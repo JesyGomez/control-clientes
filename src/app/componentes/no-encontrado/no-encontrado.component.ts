02436dd103f41c9b4adb6e6c8cb38ecf8f1cf0d7
julianycia.m@gmail.com <julianycia.m@gmail.com>
2024-04-05 14:56:12 -0300

    vinculando clases

--- src/app/componentes/no-encontrado/no-encontrado.component.ts
+++ src/app/componentes/no-encontrado/no-encontrado.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-no-encontrado',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkWithHref],
   templateUrl: './no-encontrado.component.html',
   styleUrl: './no-encontrado.component.css'
 })
