6c73f741ae4123e844d553710af19826dc47e88d
julianycia.m@gmail.com <julianycia.m@gmail.com>
2024-04-05 15:11:38 -0300

    agregando impor routerlink

--- src/app/componentes/cabecero/cabecero.component.ts
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ConfiguracionComponent } from '../configuracion/configuracion.component';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-cabecero',
   standalone: true,
-  imports: [CommonModule, ConfiguracionComponent],
+  imports: [CommonModule, ConfiguracionComponent, RouterLinkWithHref],
   templateUrl: './cabecero.component.html',
   styleUrl: './cabecero.component.css'
 })
